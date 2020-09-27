6de156c9fa2aead4d6a389996137fb44bca07482
natalia neves <natalia.neves@totvs.com.br>
2020-09-27 20:40:19 -0300

    ALterações

--- src/app/tela-inicial/tela-inicial.component.ts
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -1,5 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+export interface IRestaurante {
+  cardapios: ICardapios[];
+  id: string;
+  localizacao: ILocalizacoes;
+  nome: string;
+  restricoes:
+  'NAO_CONTEM_GLUTEN' |
+  'NAO_CONTEM_LACTOSE' |
+  'VEGANO' |
+  'VEGETARIANO';
+}
+
+export interface ICardapios {
+  nome: string;
+  itens: {
+    nome: string
+  }[];
+}
+
+export interface ILocalizacoes {
+  rua: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+}
 
 @Component({
   selector: 'app-tela-inicial',
@@ -12,143 +39,181 @@ export class TelaInicialComponent implements OnInit {
   paginaUsuario = false;
   listagemRestaurante = true;
 
+  naoContemGluten = false;
+  naoContemLactose = false;
+  vegetariano = false;
+  vegano = false;
+
+  editar = false;
   nomeUsuario = '';
   emailUsuario = '';
   nomeRestaurante = '';
-  cardapio: {
-    cardapios: {
-      nome: string,
-      itens: {
-        nome: string
-        restricoes:
-        'NAO_CONTEM_GLUTEN' |
-        'NAO_CONTEM_LACTOSE' |
-        'VEGANO' |
-        'VEGETARIANO'
-      }[]
+  idRestaurante = '';
+
+  cardapio: ICardapios = {
+    nome: null,
+    itens: [{
+      nome: '',
+    }],
+  };
+
+  localizacao: ILocalizacoes = {
+    rua: null,
+    bairro: null,
+    cidade: null,
+    estado: null
+  };
+
+  getAllrestaurante: IRestaurante[] = null;
+
+  constructor(
+    public httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.httpClient.get('http://localhost:8081/restaurante').subscribe(
+      (teste: IRestaurante[]) => { this.getAllrestaurante = teste; }
+    );
+  }
+
+  verificaRestricoes() {
+    const restricoes = [];
+    if (this.naoContemLactose) {
+      restricoes.push('NAO_CONTEM_LACTOSE');
     }
-  } = {
-      cardapios: {
-        nome: null,
-        itens: [{
-          nome: '',
-          restricoes: null
-        }]
-      }
-    };
-
-  localizacao: {
-    localizacao: {
-      rua: string,
-      bairro: string,
-      cidade: string,
-      estado: string
+    if (this.naoContemGluten) {
+      restricoes.push('NAO_CONTEM_GLUTEN');
     }
-  } = {
-      localizacao: {
-        rua: null,
-        bairro: null,
-        cidade: null,
-        estado: null
+    if (this.vegano) {
+      restricoes.push('VEGANO');
+    }
+    if (this.vegetariano) {
+      restricoes.push('VEGETARIANO');
+    }
+
+    return restricoes;
+  }
+
+  literal(item: string) {
+    return item === 'NAO_CONTEM_GLUTEN' ?
+      'Não contém gluten' : item === 'NAO_CONTEM_LACTOSE' ?
+        'Não contém lactose' : item === 'VEGANO' ?
+          'Vegano' : item === 'VEGETARIANO' ? 'Vegetariano' : '';
+  }
+
+  existeRestricoes(restricoes: any) {
+    this.vegano = restricoes.some((r) => r === 'VEGANO');
+    this.vegetariano = restricoes.some((r) => r === 'VEGETARIANO');
+    this.naoContemGluten = restricoes.some((r) => r === 'NAO_CONTEM_GLUTEN');
+    this.naoContemLactose = restricoes.some((r) => r === 'NAO_CONTEM_LACTOSE');
+
+    return restricoes;
+  }
+
+  trocaUsuario() {
+    this.paginaUsuario = true;
+    this.listagemRestaurante = false;
+    this.paginaRestaurante = false;
+  }
+
+  trocaRestaurante() {
+    this.paginaRestaurante = true;
+    this.paginaUsuario = false;
+    this.listagemRestaurante = false;
+  }
+
+  listaRestaurante() {
+    this.listagemRestaurante = true;
+    this.paginaUsuario = false;
+    this.paginaRestaurante = false;
+    this.httpClient.get('http://localhost:8081/restaurante').subscribe(
+      (teste: IRestaurante[]) => { this.getAllrestaurante = teste; }
+    );
+  }
+
+  salvarUsuario() {
+    this.httpClient.post('http://localhost:8081/usuario', {
+      nome: this.nomeUsuario,
+      email: this.emailUsuario
+    }).subscribe(() => this.reset());
+  }
+
+  excluirUsuario() {
+    this.httpClient.delete(`http://localhost:8081/usuario/${this.emailUsuario}`).subscribe(() => this.reset());
+  }
+
+  editarRestaurante(item: IRestaurante) {
+    this.idRestaurante = item.id;
+    this.cardapio = item.cardapios[0];
+    this.localizacao = item.localizacao;
+    this.nomeRestaurante = item.nome;
+    this.editar = true;
+    this.existeRestricoes(item.restricoes);
+  }
+
+  salvarRestaurante() {
+    if (!this.editar) {
+      this.httpClient.post('http://localhost:8081/restaurante', {
+        cardapios: [this.cardapio],
+        localizacao: this.localizacao,
+        restricoes: this.verificaRestricoes(),
+        nome: this.nomeRestaurante
+      }).subscribe(() => this.reset());
+    } else {
+      this.httpClient.put(`http://localhost:8081/restaurante/${this.idRestaurante}`, {
+        cardapios: [this.cardapio],
+        localizacao: this.localizacao,
+        restricoes: this.verificaRestricoes(),
+        nome: this.nomeRestaurante
+      }).subscribe(() => {
+        this.reset();
+        this.httpClient.get('http://localhost:8081/restaurante').subscribe(
+          (teste: IRestaurante[]) => { this.getAllrestaurante = teste; }
+        );
+        this.editar = false;
+      });
+    }
+  }
+
+  excluirRestaurante(restaurante) {
+    this.httpClient.delete(`http://localhost:8081/restaurante/${restaurante.id}`).subscribe(
+      () => {
+        this.httpClient.get('http://localhost:8081/restaurante').subscribe(
+          (teste: IRestaurante[]) => {
+            this.getAllrestaurante = teste;
+          });
+        this.reset();
       }
+    );
+  }
+
+  adicionar() {
+    this.cardapio.itens.push({
+      nome: ''
+    });
+  }
+
+  reset() {
+    this.idRestaurante = null;
+    this.cardapio = {
+      nome: null,
+      itens: [{
+        nome: '',
+      }],
     };
-
-  getAllrestaurante: {
-    cardapios: [
-      {
-        itens: {
-          nome: string,
-          restricoes: [
-            'NAO_CONTEM_GLUTEN' |
-            'NAO_CONTEM_LACTOSE' |
-            'VEGANO' |
-            'VEGETARIANO'
-          ]
-        }[];
-        nome: string
-      }
-    ],
-    id: string,
-    localizacao: {
-      bairro: string,
-      cidade: string,
-      estado: string,
-      rua: string
-    },
-    nome: string
-  }[] = null;
-
-constructor(
-  public httpClient: HttpClient
-) { }
-
-ngOnInit() {
-  this.httpClient.get('http://localhost:8081/restaurante').subscribe(
-    (teste: {cardapios: [
-      {
-        itens: {
-          nome: string,
-          restricoes: [
-            'NAO_CONTEM_GLUTEN' |
-            'NAO_CONTEM_LACTOSE' |
-            'VEGANO' |
-            'VEGETARIANO'
-          ]
-        }[];
-        nome: string
-      }
-    ],
-    id: string,
-    localizacao: {
-      bairro: string,
-      cidade: string,
-      estado: string,
-      rua: string
-    },
-    nome: string
-  }[]) => { this.getAllrestaurante = teste; }
-  );
-}
-
-trocaUsuario() {
-  this.paginaUsuario = true;
-  this.listagemRestaurante = false;
-  this.paginaRestaurante = false;
-}
-
-trocaRestaurante() {
-  this.paginaRestaurante = true;
-  this.paginaUsuario = false;
-  this.listagemRestaurante = false;
-}
-
-listaRestaurante() {
-  this.listagemRestaurante = true;
-  this.paginaUsuario = false;
-  this.paginaRestaurante = false;
-}
-
-salvarUsuario() {
-  console.log('teste');
-}
-
-excluirUsuario() {
-  console.log('teste');
-}
-
-salvarRestaurante() {
-  console.log('teste');
-}
-
-excluirRestaurante() {
-  console.log('teste');
-}
-
-adicionar() {
-  this.cardapio.cardapios.itens.push({
-    nome: '',
-    restricoes: null
-  });
-}
+    this.localizacao = {
+      rua: null,
+      bairro: null,
+      cidade: null,
+      estado: null
+    };
+    this.nomeRestaurante = null;
+    this.nomeUsuario = null;
+    this.emailUsuario = null;
+    this.naoContemLactose = false;
+    this.naoContemLactose = false;
+    this.vegetariano = false;
+    this.vegano = false;
+  }
 
 }
